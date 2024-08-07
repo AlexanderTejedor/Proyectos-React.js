c77a901a5a8da059cde2f44ec6da15de7c0acf3d
Alexander Tejedor <atromero.98@gmail.com>
2024-08-06 22:31:30 -0500

    Obtimizando codigo, y estilizando

---
+++ projects/02-tic-tac-toe/src/Components/Square.jsx
@@ -0,0 +1,13 @@
+/*Tablero del cuadrado - Componente*/
+export const Square = ({ children, isSelected, updateBoard, index }) => {
+    const className = `square ${isSelected ? 'is-selected' : ''}`
+    
+    const handleClick = () => {
+        updateBoard(index)
+    }
+    return (
+        <div onClick={handleClick} className={className}>
+        {children}
+        </div>
+    )
+}
